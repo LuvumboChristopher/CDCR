7e91f9e5d48680089ce6a42d5ecb2ccbae79400d
luvumbochristopher <christopherluvumbo@gmail.com>
2023-03-21 19:16:46 +0100

    Mayor changes in the estructure

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,7 @@ const menu = document.querySelector(".nav-responsive-container");
 const ham = document.querySelector(".menu-burger");
 const xIcon = document.querySelector(".xIcon");
 const menuIcon = document.querySelector(".open");
+const links = document.querySelectorAll('.nav__link')
 
 ham.addEventListener("click", toggleMenu)
 
@@ -16,3 +17,13 @@ function toggleMenu() {
         menuIcon.style.display = "none";
     }
 }
+
+links.forEach(link => {
+    console.log(link.href, window.location.href);
+    console.log('estoy aqui');
+    if(link.href === window.location.href){
+        console.log("estoy en la pagina")
+      link.setAttribute('aria-current', 'page')
+      console.log(link)
+    }
+  })
