89075c5e4313cc14b7dd264e49d70377135dd034
luvumbochristopher <christopherluvumbo@gmail.com>
2023-03-26 00:29:40 +0100

    vercel deployment

--- views/partials/footer/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="footer-container-box">
-       <p class="footer-text ">CDCR © <script>document.write(new Date().getFullYear())</script> Tous droits réservés.</p>
-    </div>
-</footer>
